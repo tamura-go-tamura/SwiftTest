a95be2691b4e3e1cebb175143070a3eee38d8ac4
tamura-go-tamura <tamura.go.tamura@gmail.com>
2024-05-29 19:30:57 +0900

    Initial Commit

---
+++ SwiftTest/SwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestApp.swift
+//  SwiftTest
+//
+//  Created by yuta on 2024/05/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
