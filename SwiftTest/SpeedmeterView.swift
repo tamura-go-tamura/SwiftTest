97dbb710392a7e752a951ba40e4e051e2be179a9
Yuta Tanikawa <yuta.tanikawa@sslab.ics.keio.ac.jp>
2024-05-29 19:48:22 +0900

    first commit

---
+++ SwiftTest/SpeedmeterView.swift
@@ -0,0 +1,25 @@
+//
+//  SpeedmeterView.swift
+//  SwiftTest
+//
+//  Created by 谷川優太 on 2024/05/29.
+//
+
+import Foundation
+import SwiftUI
+struct SpeedmeterView: View {
+ 
+    @State private var currentSpeed = 140.0
+ 
+    var body: some View {
+        Gauge(value: currentSpeed, in: 0...200) {
+            Image(systemName: "gauge.medium")
+                .font(.system(size: 50.0))
+        } currentValueLabel: {
+            Text("\(currentSpeed.formatted(.number))")
+ 
+        }
+        .gaugeStyle(SpeedometerGaugeStyle())
+ 
+    }
+}
